98042e421c575074387980786edb3cdc6257112a
Rafael Vargas <vargas718@gmail.com>
2019-01-09 05:36:41 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,8 +1,10 @@
 
 def levitation_quiz
   levitation = 0 
-  
-	puts "What is the spell that enacts levitation?"
+  until 
+  puts "What is the spell that enacts levitation?"
+  levitation += 1
+  break if levitation gets ""
 end
-puts 
+puts "You passed the quiz!"
 
